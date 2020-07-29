307daf3edba4bad4ef332840e2f9e9779b6bc0b2
acuicultor <ramoncuenca4@gmail.com>
2020-07-29 13:08:20 +0200

    cpufreq: schedutil: Relax schedutil up/down rate limit thresholds

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1320,8 +1320,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 	tunables->iowait_boost_enable = 1;
 
 	/* Hard-code some sane rate-limit values */
-	tunables->up_rate_limit_us = 500;
-	tunables->down_rate_limit_us = 20000;
+	tunables->up_rate_limit_us = 5000;
+	tunables->down_rate_limit_us = 10000;
 
 	switch (policy->cpu) {
 	default:
