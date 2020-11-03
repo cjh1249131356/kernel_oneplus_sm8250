2d71ce7816fc15651379298aeb34ab752c5633a8
LibXZR <xzr467706992@163.com>
2020-11-03 13:18:13 +0800

    cpu_input_boost: Expose screen status
    * Needed to make cpu up & down cooperate perfectly with fod

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -21,6 +21,8 @@
 #include <uapi/linux/sched/types.h>
 #endif
 
+bool is_screen_on = true;
+
 static unsigned int input_boost_freq_little __read_mostly =
 	CONFIG_INPUT_BOOST_FREQ_LP;
 static unsigned int input_boost_freq_big __read_mostly =
@@ -329,10 +331,12 @@ static int msm_drm_notifier_cb(struct notifier_block *nb, unsigned long action,
 	/* Boost when the screen turns on and unboost when it turns off */
 	if (*blank == MSM_DRM_BLANK_UNBLANK_CUST) {
 		set_bit(SCREEN_ON, &b->state);
+		is_screen_on = true;
 		do_cpu_updown(true);
 		__cpu_input_boost_kick_max(b, wake_boost_duration);
 	} else if (*blank == MSM_DRM_BLANK_POWERDOWN_CUST) {
 		clear_bit(SCREEN_ON, &b->state);
+		is_screen_on = false;
 		wake_up(&b->boost_waitq);
 		do_cpu_updown(false);
 	}
